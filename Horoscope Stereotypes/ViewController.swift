186013aea1b37d7c97e2f9faa19dd2d2f4961977
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-25 15:37:51 -0400

    computer froze

--- Horoscope Stereotypes/ViewController.swift
+++ Horoscope Stereotypes/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        AlertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+    }
 
 
 }
