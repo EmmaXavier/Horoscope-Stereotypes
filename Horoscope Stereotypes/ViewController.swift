64c0694f698e73a3711550f47b6cd6e9328a01b0
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-25 16:17:00 -0400

    Iteration 3

--- Horoscope Stereotypes/ViewController.swift
+++ Horoscope Stereotypes/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let emojis = ["♈️" : ["People think aries have a short fuse, but people just can't comprehend their passion", "The small minded will say aries are confrontational, but really, they just know what they want", "People must be confused, they think aries leave things unfinished, yet they have unrivaled passion? Uh... okay."],  "♉️" : ["People frame the stubborness of tarus as negative, but they just won't stop until they reah their goals", "They say taurus are lazy, but have they seen The Rock? That's one jacked taurus!", "People think taurus are selfish, yet also generous? Maybe taurus are just capable of selflessness *and* self love"], "♊️" : ["Gemini are", "Gemini can be"]]
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,7 +18,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessage(sender: UIButton) {
+        
+        let selectEmotion = sender.titleLabel?.text
+        
         let alertController = UIAlertController()
+        emojis.values
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
         
