6c181a49ef1d3b283a334d07a43bad2f36c90d74
Apple <apple@Apples-MacBook-Pro-3.local>
2019-07-26 09:40:24 -0400

    Working! Now I just need the other 9

--- Horoscope Stereotypes/ViewController.swift
+++ Horoscope Stereotypes/ViewController.swift
@@ -16,21 +16,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    let emojis = ["♈️" : "Aries",  "♉️" : "Taurus", "♊️" : "Gemini"]
+
     
-    let customMessage = ["♈️": ["People think aries have a short fuse, but people just can't comprehend their passion", "The small minded will say aries are confrontational, but really, they just know what they want", "People must be confused, they think aries leave things unfinished, yet they have unrivaled passion?Uh... okay."], "♉️": ["People frame the stubborness of tarus as negative, but they just won't stop until they reah their goals", "They say taurus are lazy, but have they seen The Rock? That's one jacked taurus!", "People think taurus are selfish, yet also generous? Maybe taurus are just capable of selflessness *and* self love"], "♊️": ["Gemini are", "Gemini can be"]]
 
 
     @IBAction func showMessage(sender: UIButton) {
         
+        let emojis = ["♈️" : "Aries",  "♉️" : "Taurus", "♊️" : "Gemini"]
+        let customMessage = ["Aries": ["People think aries have a short fuse, but people just can't comprehend their passion", "The small minded will say aries are confrontational, but really, they just know what they want", "People must be confused, they think aries leave things unfinished, yet they have unrivaled passion?Uh... okay."], "Taurus": ["People frame the stubborness of tarus as negative, but they just won't stop until they reah their goals", "They say taurus are lazy, but have they seen The Rock? That's one jacked taurus!", "People think taurus are selfish, yet also generous? Maybe taurus are just capable of selflessness *and* self love"], "Gemini": ["Gemini are", "Gemini can be"]]
+
+        
         let randomize = Int.random(in: 0 ..< 3)
         let selectedSign = sender.titleLabel?.text
-        let signArray = customMessage[emojis[selectedSign!]!]
-        let emojiMessage = signArray?[random]
+        let emojiMessage = customMessage[emojis[selectedSign!]!]?[randomize]
         
-        let alertController = UIAlertController(title: selectedSign)
-        emojiMessage
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: emojis[selectedSign!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Bet", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
         
     }
